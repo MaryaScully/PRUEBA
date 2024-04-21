617d8abf0c687d5fc293a896dfb86ffe098767e6
MaryaScully <maryaestanislaogomez@gmail.com>
2024-04-21 19:33:05 +0200

    Update IntroBBDD.sql

---
+++ IntroBBDD.sql
@@ -0,0 +1,21 @@
+SELECT aircraft_code, status
+FROM flights
+WHERE status = 'On Time'
+
+
+SELECT *
+FROM bookings
+WHERE total_amount >= 1000000
+
+
+SELECT *
+FROM aircrafts_data
+
+
+SELECT *
+FROM aircrafts_data
+
+
+SELECT *
+FROM tickets
+WHERE passenger_name like '%IRINA%'
